5e4851065d6c794d3bc12a4034c747abf6d462f7
RohanRony <rohanrony@hotmail.com>
2021-03-08 18:42:29 +0300

    scroll-animation-functions

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ $(document).ready(function() {
       scrollbars: true,
       standardScrollElements: "",
       setHeights: true,
-      overflowScroll: true,
+      overflowScroll: false,
       updateHash: true,
       touchScroll: true,
       before: function() {},
-      after: function() {},
+      after: function() {ContentAnimation()},
       afterResize: function() {},
       afterRender: function() {}
     });
@@ -47,156 +47,25 @@ $(document).ready(function() {
       });
     });
   });
-
-
-dot1.addEventListener("click", ()=>
-{   
-    // svg.style.backgroundColor='#3d5af1';
-
-    svgring.style.strokeDasharray="0,1000"
-
-    // page1.style.display='block';
-    // page2.style.display='none';
-    // page3.style.display='none';
-    // page4.style.display='none';
-    // page5.style.display='none';
-    // page6.style.display='none';
-    // page7.style.display='none';
-
+function ContentAnimation()
+{
     gsap.from(".h1", {
         opacity: 0, 
         y: 30, 
-        duration: 1,
+        duration: 1.2,
         ease: 'power3.out'
       });
       gsap.from(".run", {
         opacity: 0, 
         y: -30, 
-        duration: 1,
+        duration: 1.2,
         ease: 'power3.out'
       });
-      gsap.from("#img1", {duration: 1, y:"-100%"})
+    gsap.from("#img1", {duration: 1, y:"-100%"})
     gsap.from("#img2", {duration: 1, y:"100%"})
-    
-    
-    for (var j= 6; j>0; j--)
-    {
-        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
-        
-    }
-    
-})
-
-dot2.addEventListener("click", ()=>
-{   
-  // svg.style.backgroundColor='#153e90';
-
-  svgring.style.strokeDasharray="130,1000"
-
-  // page1.style.display='none';
-  // page2.style.display='block';
-  // page3.style.display='none';
-  // page4.style.display='none';
-  // page5.style.display='none';
-  // page6.style.display='none';
-  // page7.style.display='none';
-
-  gsap.from(".h1", {
-    opacity: 0, 
-    y: 30, 
-    duration: 1,
-    ease: 'power3.out'
-  });
-  gsap.from(".run", {
-    opacity: 0, 
-    y: -30, 
-    duration: 1,
-    ease: 'power3.out'
-  });
-  gsap.from("#right-section", {duration:0.85, y: "100%"})
-    
-    for (var i= 0; i<=1; i++)
-    {
-        dotarray[i].firstElementChild.firstElementChild.style.fill='rgb(0, 146, 255)';
-        
-    }
-    for (var j= 6; j>1; j--)
-    {
-        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
-        
-    }
-
-    
-    
-})
-dot3.addEventListener("click", ()=>
-{   
-
-    svgring.style.strokeDasharray="280,1000"
-    // svg.style.backgroundColor = "#3e044d";
-  // page1.style.display='none';
-  // page2.style.display='none';
-  // page3.style.display='block';
-  // page4.style.display='none';
-  // page5.style.display='none';
-  // page6.style.display='none';
-  // page7.style.display='none';
-
-  gsap.from(".h1", {
-    opacity: 0, 
-    y: 30, 
-    duration: 1,
-    ease: 'power3.out'
-  });
-  gsap.from(".run", {
-    opacity: 0, 
-    y: -30, 
-    duration: 1,
-    ease: 'power3.out'
-  });
-  gsap.from("#page-three-firstpic", {duration: 1, y:"100%"})
-        gsap.from("#page-three-secondpic", {duration: 1, y:"-100%"})
-    
-    for (var i= 0; i<=2; i++)
-    {
-        
-        dotarray[i].firstElementChild.firstElementChild.style.fill='rgb(0, 146, 255)';
-      
-    }
-    for (var j= 6; j>2; j--)
-    {
-        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
-        
-    }
-   
-})
-dot4.addEventListener("click", ()=>
-{   
-    // svg.style.backgroundColor='#2e7447';
-
-    svgring.style.strokeDasharray="420,1000"
-
-    svg.style.backgroundColor=
-    // page1.style.display='none';
-    // page2.style.display='none';
-    // page3.style.display='none';
-    // page4.style.display='block';
-    // page5.style.display='none';
-    // page6.style.display='none';
-    // page7.style.display='none';
-
-    gsap.from(".h1", {
-        opacity: 0, 
-        y: 30, 
-        duration: 1,
-        ease: 'power3.out'
-      });
-      gsap.from(".run", {
-        opacity: 0, 
-        y: -30, 
-        duration: 1,
-        ease: 'power3.out'
-      });
+    gsap.from(".right-section", {duration:1, y: "100%"})
+    gsap.from("#page-three-firstpic", {duration: 1, y:"100%"})
+    gsap.from("#page-three-secondpic", {duration: 1, y:"-100%"})
     gsap.from("#g-1", {
         opacity: 0, 
         y: 100, 
@@ -207,48 +76,7 @@ dot4.addEventListener("click", ()=>
         y: 100, 
         duration: 1
       });
- 
-    for (var i= 0; i<=3; i++)
-    {
-        
-        dotarray[i].firstElementChild.firstElementChild.style.fill='rgb(0, 146, 255)';
-    }
-    for (var j= 6; j>3; j--)
-    {
-        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
-        
-    }
-   
-})
-dot5.addEventListener("click", ()=>
-{   
-
-    // svg.style.backgroundColor='#1f45fc';
-
-    svgring.style.strokeDasharray="530,1000";
-    svgring.style.animation="dash 2s linear forwards"
-    
-    // page1.style.display='none';
-    // page2.style.display='none';
-    // page3.style.display='none';
-    // page4.style.display='none';
-    // page5.style.display='block';
-    // page6.style.display='none';
-    // page7.style.display='none';
-
-    gsap.from(".h1", {
-        opacity: 0, 
-        y: 30, 
-        duration: 1,
-        ease: 'power3.out'
-      });
-      gsap.from(".run", {
-        opacity: 0, 
-        y: -30, 
-        duration: 1,
-        ease: 'power3.out'
-      });
-    gsap.from("#b-1", {
+      gsap.from("#b-1", {
         opacity: 0, 
         y: 100, 
         duration: 0.5
@@ -273,7 +101,114 @@ dot5.addEventListener("click", ()=>
         y: 100, 
         duration: 0.5
       });
+      gsap.from("#page-six-firstpic", {
+        opacity: 0, 
+        x: -100, 
+        duration: 1
+      });
+      gsap.from("#page-six-secondpic", {
+        opacity: 0, 
+        y: -100, 
+        duration: 1
+      });
+      gsap.from("#page-six-thirdpic", {
+        opacity: 0, 
+        y: 100, 
+        duration: 1
+      });
+      gsap.from("#page-six-fourthpic", {
+        opacity: 0, 
+        y: 100,
+     
+        duration: 1
+      });
+      gsap.from("#page-six-fifthpic", {
+        opacity: 0, 
+        y: 100, 
+        x:100,
+        duration: 1
+      });
+      gsap.from("#o1", {
+        opacity: 0, 
+        y: -100, 
+        duration: 0.7
+      });
+    gsap.from("#o2", {
+        opacity: 0, 
+        y: 100, 
+        duration: 0.7
+      });
+}
+function animation1()
+{
+    svgring.style.strokeDasharray="0,1000"
+    for (var j= 6; j>0; j--)
+    {
+        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
+        
+    }
+}
+
+function animation2()
+{
+    svgring.style.strokeDasharray="130,1000"
+
   
+    
+    for (var i= 0; i<=1; i++)
+    {
+        dotarray[i].firstElementChild.firstElementChild.style.fill='rgb(0, 146, 255)';
+        
+    }
+    for (var j= 6; j>1; j--)
+    {
+        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
+        
+    }
+}
+
+function animation3()
+{
+    svgring.style.strokeDasharray="280,1000"
+ 
+    
+    for (var i= 0; i<=2; i++)
+    {
+        
+        dotarray[i].firstElementChild.firstElementChild.style.fill='rgb(0, 146, 255)';
+      
+    }
+    for (var j= 6; j>2; j--)
+    {
+        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
+        
+    }
+   
+}
+
+function animation4()
+{
+    svgring.style.strokeDasharray="420,1000"
+
+    
+    for (var i= 0; i<=3; i++)
+    {
+        
+        dotarray[i].firstElementChild.firstElementChild.style.fill='rgb(0, 146, 255)';
+    }
+    for (var j= 6; j>3; j--)
+    {
+        dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
+        
+    }
+
+}
+
+function animation5()
+{
+    svgring.style.strokeDasharray="530,1000";
+    
+
     for (var i= 0; i<=4; i++)
     {
         
@@ -284,63 +219,13 @@ dot5.addEventListener("click", ()=>
         dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
         
     }
-    
-})
-dot6.addEventListener("click", ()=>
-{   
-    // svg.style.backgroundColor = "#1a2639";
+}
 
+function animation6()
+{
     svgring.style.strokeDasharray="680,1000";
-    svgring.style.animation="dash 2s linear forwards"
-
-  //   page1.style.display='none';
-  // page2.style.display='none';
-  // page3.style.display='none';
-  // page4.style.display='none';
-  // page5.style.display='none';
-  // page6.style.display='block';
-  // page7.style.display='none';
-
-
-  gsap.from(".h1", {
-    opacity: 0, 
-    y: 30, 
-    duration: 1,
-    ease: 'power3.out'
-  });
-  gsap.from(".run", {
-    opacity: 0, 
-    y: -30, 
-    duration: 1,
-    ease: 'power3.out'
-  });
-  gsap.from("#page-six-firstpic", {
-    opacity: 0, 
-    x: -100, 
-    duration: 1
-  });
-  gsap.from("#page-six-secondpic", {
-    opacity: 0, 
-    y: -100, 
-    duration: 1
-  });
-  gsap.from("#page-six-thirdpic", {
-    opacity: 0, 
-    y: 100, 
-    duration: 1
-  });
-  gsap.from("#page-six-fourthpic", {
-    opacity: 0, 
-    y: 100,
- 
-    duration: 1
-  });
-  gsap.from("#page-six-fifthpic", {
-    opacity: 0, 
-    y: 100, 
-    x:100,
-    duration: 1
-  });
+   
+  
 
     for (var i= 0; i<=5; i++)
     {
@@ -352,45 +237,14 @@ dot6.addEventListener("click", ()=>
         dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
         
     }
-    
-})
-dot7.addEventListener("click", ()=>
-{   
-    // svg.style.backgroundColor="#00003f"
-    // page1.style.display='none';
-    // page2.style.display='none';
-    // page3.style.display='none';
-    // page4.style.display='none';
-    // page5.style.display='none';
-    // page6.style.display='none';
-    // page7.style.display='block';
+}
 
+function animation7()
+{
     svgring.style.strokeDasharray="820,1000";
-    svgring.style.animationDuration='200';
+    
 
-    gsap.from(".h1", {
-        opacity: 0, 
-        y: 30, 
-        duration: 1,
-        ease: 'power3.out'
-      });
-    gsap.from(".run", {
-        opacity: 0, 
-        y: -30, 
-        duration: 1,
-        ease: 'power3.out'
-      });
-    gsap.from("#o1", {
-        opacity: 0, 
-        y: -100, 
-        duration: 1
-      });
-    gsap.from("#o2", {
-        opacity: 0, 
-        y: 100, 
-        duration: 1
-      });
-
+   
     for (var i= 0; i<=6; i++)
     {
         
@@ -401,6 +255,35 @@ dot7.addEventListener("click", ()=>
         dotarray[j].firstElementChild.firstElementChild.style.fill='rgb(255,255,255)';
         
     }
-})
+}
 
+dot1.addEventListener("click", animation1)
+dot2.addEventListener("click", animation2)
+dot3.addEventListener("click", animation3)
+dot4.addEventListener("click", animation4)
+dot5.addEventListener("click", animation5)
+dot6.addEventListener("click", animation6)
+dot7.addEventListener("click", animation7)
 
+window.addEventListener('scroll',(e)=>{
+    e.preventDefault();
+    const scroll=window.scrollY;
+    
+    if(scroll==0){
+      animation1();
+    }else if(scroll>600 && scroll<790){
+      animation2();
+    }
+    else if(scroll>770 && scroll<1540){
+      animation3();
+  
+    }else if(scroll>1550 && scroll<2300){
+      animation4();
+    }else if(scroll>2310 && scroll<3060){
+      animation5();
+    }else if(scroll>3080 && scroll<3820){
+      animation6();
+    }else if(scroll>3840 && scroll<4590){
+      animation7();
+    }
+  })
